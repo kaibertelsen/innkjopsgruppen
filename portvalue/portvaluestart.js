4c1aaa1db520e49c3a6b86de71b2eba6e7cf86fb
Kai Bertelsen <kai@exposoft.no>
2024-11-27 15:22:35 +0100

    ny

--- portvalue/portvaluestart.js
+++ portvalue/portvaluestart.js
@@ -1,16 +1,12 @@
 
 function getKlientdata(){
-
-let klientid = "rec1QGUGBMVaqxhp1";
+    let klientid = "rec1QGUGBMVaqxhp1";
     GETairtable("app1WzN1IxEnVu3m0","tbldZL68MyLNBRjQC",klientid,"klientresponse")
 }
 
 function klientresponse(data){
     console.log(data);
 }
-
-
-
 
 
 
