41fef8845f1ee4cf829eb8afbc09114041795093
Kai Bertelsen <kai@exposoft.no>
2025-02-11 09:42:47 +0100

    ny

--- app/apploader.js
+++ app/apploader.js
@@ -13,6 +13,7 @@ const cdnScripts = [
     "https://kaibertelsen.github.io/innkjopsgruppen/app/appstartup.js",
     "https://kaibertelsen.github.io/innkjopsgruppen/app/elementtriggers.js",
     "https://kaibertelsen.github.io/innkjopsgruppen/app/filterlist.js",
+    "https://kaibertelsen.github.io/innkjopsgruppen/app/companyprofile.js",
     "https://kaibertelsen.github.io/innkjopsgruppen/apicomcurrent.js"
 ];
 
---
+++ app/companyprofile.js
@@ -0,0 +1,76 @@
+function companyPageChosed(company) {
+   
+
+    // Simulerer klikk på elementet
+    document.getElementById("tabcompany").click();
+
+    const conteinerpage = document.getElementById("companyconteinerpage");
+
+      // Sett logo
+      const logo = conteinerpage.querySelector('.subpagelogo');
+      if (logo) {
+          if (company.logo) {
+              logo.src = company.logo;
+              logo.style.display = "inline-block";
+          } else {
+            logo.style.display = "none";
+          }
+      }
+    
+    // Oppdaterer data på selskapet
+    let name = conteinerpage.querySelector('.companyname');
+    name.textContent = company.Name || "-";
+
+    let orgnr = conteinerpage.querySelector('.orgnr');
+    orgnr.textContent = company.orgnr || "-";
+
+    let groupname = conteinerpage.querySelector('.group');
+    groupname.textContent = company.groupname || "-";
+
+    let adress = conteinerpage.querySelector('.adress');
+    adress.textContent = company.adresse || "-";
+
+    let post = conteinerpage.querySelector('.post');
+    adress.textContent = company.postnr+" "+company.poststed;
+
+
+      let users = company.bruker;
+      const list = document.getElementById("memberholderlist");
+     
+      if (!list) {
+          console.error("Ingen 'elementlibrary' funnet.");
+          return;
+      }
+      list.innerHTML = '';
+      
+      const elementLibrary = document.getElementById("elementlibrary");
+      if (!elementLibrary) {
+          console.error("Ingen 'elementlibrary' funnet.");
+          return;
+      }
+  
+      const nodeElement = elementLibrary.querySelector(".membercardwrapper");
+      if (!nodeElement) {
+          console.error("Ingen '.suppliercard' funnet i 'elementlibrary'.");
+          return;
+      }
+
+
+
+      // Oppdater med nye leverandører
+    users.forEach(member => {
+        const memberElement = nodeElement.cloneNode(true);
+
+        let name = memberElement.querySelector('.name');
+        name.textContent = member.navn || "-";
+
+        let email = memberElement.querySelector('.email');
+        email.textContent = member.epost || "-";
+
+        let roll = memberElement.querySelector('.roll');
+        roll.textContent = member.rolle || "-";
+
+        list.appendChild(memberElement);
+
+    });
+}
