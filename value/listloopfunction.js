f435a69eff9d68d00ac50414ac13d252142f8c56
Kai Bertelsen <kai@exposoft.no>
2025-02-20 09:10:04 +0100

    ny

--- value/listloopfunction.js
+++ value/listloopfunction.js
@@ -124,3 +124,135 @@ function listElements(data,list,type){
             
     return {sumvalue:gvalue,sumcutvalue:gcut,sumbvalue:bvalue,sumavalue:avalue};         
 }
+function mergesuppiersCachflow(data){
+ 
+    var mergearray = [];
+    
+        for (var i = 0;i<data.length;i++){
+            //denne skal kun slå sammen handelstyper
+         
+            var objects = findObjectsProperty("suppliername",data[i].suppliername[0],mergearray);
+            let cut;
+            if(data[i]?.localcut){
+              cut = data[i].localcut;   
+            }else if(data[i]?.defaultcut){
+              cut = data[i].defaultcut[0];    
+            }else{
+             cut = 0;   
+            }
+            
+            if(objects){
+                //objecter funnet med "samme navn"
+                //finne samme cut
+                var cutobject = findObjectProperty("cut",cut,objects);
+                
+                if(cutobject){
+                    //samme cut
+                     cutobject.value = Number(cutobject.value)+Number(data[i].value);
+                     //legge til en linje
+                     cutobject.lines = Number(cutobject.lines)+1;
+                }else{
+                    //ikke samme cut
+                    let newLine = {suppliername:data[i].suppliername[0],value:data[i].value,cut:cut,lines:1,airtable:data[i].airtable};
+
+                    if(data[i]?.supplierquantityname[0]){
+                        newLine.quantityname = data[i].supplierquantityname[0];
+                    }
+                    mergearray.push(newLine);
+                }
+      
+                
+            }else{
+                let newLine = {suppliername:data[i].suppliername[0],value:data[i].value,cut:cut,lines:1,airtable:data[i].airtable};
+                if(data[i]?.supplierquantityname[0]){
+                    newLine.quantityname = data[i].supplierquantityname[0];
+                }
+                //denne linjeb har localcut
+                mergearray.push(newLine);
+            }
+            
+        }
+    return sortArrayABC("suppliername",mergearray);
+    }
+{
+    "nr": 19411,
+    "airtable": "rec6v2vc8ZpLE6QJI",
+    "value": 1951.069999999999,
+    "supplier": [
+        "recSW81Klal73BRHv"
+    ],
+    "customer": [
+        "rec6jAr1tU9rDw32i"
+    ],
+    "import": [
+        "recntB5z5MtHDydt1"
+    ],
+    "periodeend": [
+        "2025-01-31"
+    ],
+    "periodestart": [
+        "2025-01-01"
+    ],
+    "importname": [
+        "VolumInnkjpsgruppen12025.xlsx"
+    ],
+    "customername": [
+        "Viken Betong AS"
+    ],
+    "tracking": "webimporter",
+    "rowindex": 5,
+    "defaultcut": [
+        0.01
+    ],
+    "dachboard": [
+        "recfJZ0PCPOrWcBLq"
+    ],
+    "customerid": [
+        "rec6jAr1tU9rDw32i"
+    ],
+    "suppliername": [
+        "UnoX"
+    ],
+    "cutvalue": 195.1069999999999,
+    "type": "handel",
+    "Last Modified": "2025-02-06T13:47:22.000Z",
+    "analysevalue": 0,
+    "bistandvalue": 0,
+    "kickback": [
+        0.05
+    ],
+    "kickbackvalue": 78.04279999999996,
+    "maindate": [
+        "2025-01-31"
+    ],
+    "seconddate": [
+        "2025-01-31"
+    ],
+    "optionname": [
+        "UnoX"
+    ],
+    "supplierid": [
+        "recSW81Klal73BRHv"
+    ],
+    "cleansupplierid": [
+        "recSW81Klal73BRHv"
+    ],
+    "cleanmark": "-",
+    "customergroup": [
+        "recU4I3Q8PsPKg2or"
+    ],
+    "customergroupname": [
+        "Bygg og Anlegg"
+    ],
+    "quantity": 1951.069999999999,
+    "supplierquantity": [
+        "recxZnfBl6xY8EUXQ"
+    ],
+    "supplierkickbackperquantity": [
+        0.04
+    ],
+    "suppliersavingsperquantity": [
+        0.1
+    ],
+    "json": "{\"kickbackvalue\": \"78.04279999999996\",\"value\": \"1951.069999999999\",\"airtable\": \"rec6v2vc8ZpLE6QJI\",\"customer\": \"Viken Betong AS\",\"supplier\": \"UnoX\",\"group\": \"recU4I3Q8PsPKg2or\",\"groupname\": \"Bygg og Anlegg\",\"cut\": \"195.1069999999999\",\"bistand\": \"0\",\"analyse\": \"0\",\"maindate\": \"2025-01-31T00:00:00.000Z\"}"
+}
