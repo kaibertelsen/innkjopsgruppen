6a8d35ee7cc418c262f62e538781eb1842c39f8a
Kai Bertelsen <kai@exposoft.no>
2025-03-21 14:10:50 +0100

    ny

--- portvalue/liquidity.js
+++ portvalue/liquidity.js
@@ -553,7 +553,7 @@ function loadLiquidityExitOverview(data) {
 
         // Animasjon for andre element
         const second = monthElement.querySelector(".second");
-        second.classlist.add("readcolor");
+        second.classList.add("redcolor");
         const secondText = monthElement.querySelector(".secondtextlable");
         secondText.style.fontWeight = "bold";
         let heightSecond = secondValue / factorHeight;
