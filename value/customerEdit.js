33cd0ba3109e16ed15563e14bbf149eb18f42d74
Kai Bertelsen <kai@exposoft.no>
2025-03-25 13:20:18 +0100

    ny

--- value/customerEdit.js
+++ value/customerEdit.js
@@ -212,3 +212,275 @@ function respondCompanyCutSettings(data){
     SelectedCompanyInFirstTab.cutsettings.push(datajson);
     listcompanycutsettings(SelectedCompanyInFirstTab.cutsettings);
 }
+
+function addnewline(type,buttonid){
+    reseteditwrapperinput();
+    showhideeditwraper(type);
+    const element = document.getElementById("editornewwrapper");
+    element.style.display = "block";
+    var listid;
+    if(type == "handel"){
+        listid = "listcopyholder";
+    }else if(type == "bistand"){
+        listid = "listholderbistand";
+    }else if(type == "analyse"){
+        listid = "listholderanalyse";
+    }
+    const list= document.getElementById(listid);
+    list.appendChild(element);
+    //document.getElementById(buttonid).style.display = "none";
+}
+
+function editLine(id,elementid,tracking){
+        
+        const element = document.getElementById("editornewwrapper");
+        
+        if(element.dataset?.hideobject){
+            if(document.getElementById(element.dataset.hideobject)){
+            //synligjør tidligere skjult element
+            document.getElementById(element.dataset.hideobject).style.display = "grid";
+            }
+        }
+        
+        var object = findObjectProperty("airtable",id,companydatalines);
+        loadEditwrapper(object);
+        
+        
+        if(tracking == "handel"){
+          //handelsoppsett  
+       
+        }
+        
+        const referanseelement = document.getElementById(elementid);
+        insertElementInline(referanseelement,element);
+        
+        //synligjør editbar
+        document.getElementById("editornewwrapper").style.display = "block";
+        
+        //skjuler editlinje
+        referanseelement.style.display = "none";
+        element.dataset.hideobject = referanseelement.id;
+    
+    
+}
+     
+function insertElementInline(referanseelement,element){
+     // Get the list and the reference item
+            var list = referanseelement.parentElement;
+    
+            // Insert the new item after item 2
+            if (referanseelement.nextSibling) {
+                list.insertBefore(element, referanseelement.nextSibling);
+            } else {
+                list.appendChild(element);
+            }
+}
+    
+function cancleEdit(){
+    const element = document.getElementById("editornewwrapper");
+    if(element.dataset.hideobject){
+            //synligjør tidligere skjult element
+            document.getElementById(element.dataset.hideobject).style.display = "grid";
+    }
+    
+    element.style.display = "none";
+    //reseteditwrapperinput();
+    //ny linjeknappen
+    document.getElementById("addnewlinehandelbutton").style.display = "inline-block";
+    document.getElementById("addnewlinebistandbutton").style.display = "inline-block";
+}
+    
+function deleteLine(airtable){
+    
+    DELETEairtable(baseid,"tblkNYRENn5QFG0CD",airtable,"responsdeletevolum");
+      
+    //synligjør melding
+    document.getElementById("savinglinewrapper").style.display = "flex";
+    document.getElementById("prosessmessagetext").innerHTML = "Sletter ..."
+    
+    //skjule data
+    document.getElementById("maineditrow").style.display = "none";
+    document.getElementById("infoinputwrapper").style.display = "none";
+}
+
+function loadEditwrapper(data){
+    showhideeditwraper(data.type);
+    //wrapper
+    const element = document.getElementById("editornewwrapper");
+    element.dataset.airtable = data.airtable;
+    
+        const supplierinput = document.getElementById("dropdownInputsupplier");
+        if(data?.suppliertext){
+        supplierinput.value = data.suppliertext;    
+        }else{
+        //supplier
+        supplierinput.value = data.suppliername;
+        supplierinput.dataset.airtable = data.supplier[0];
+        }
+     
+     
+     
+     if(data.type == "handel"){
+     //value
+     document.getElementById("valueinput").value = valutalook(round(data.value,0))+" Kr";
+     //cut
+     var cut = 0;
+     if(data?.localcut){
+        cut = data.localcut;   
+        }else if(data?.defaultcut){
+        cut = data.defaultcut[0];    
+        }
+     document.getElementById("cutinput").value = round(Number(cut)*100, 2)+"%";
+     //cutvalue
+     document.getElementById("cutvalueinput").innerHTML = valutalook(round(Number(data.value)*Number(cut),0))+" Kr";
+     }else if (data.type == "bistand"){
+      //mark
+            var mark = "";
+            if(data?.mark){
+             mark = data.mark; 
+            }
+      document.getElementById("markinput").value = mark;
+      document.getElementById("bvalueinput").value = valutalook(round(data.bistandvalue,0))+" Kr";
+     }else if (data.type == "analyse"){
+      //mark
+            var mark = "";
+            if(data?.mark){
+             mark = data.mark; 
+            }
+      document.getElementById("markinput").value = mark;
+      document.getElementById("avalueinput").value = valutalook(round(data.analysevalue,0))+" Kr";
+     }
+     
+     
+     
+     //datevolum
+     var date = "";
+                    if(data?.date){
+                    date =  data.date;
+                    }else if(data?.periodeend){
+                    date = data.periodeend;
+                    }
+     document.getElementById("datevolum").value = date;
+     //note
+     var note = "";
+     if(data?.note){
+        note = data.note; 
+     }
+     document.getElementById("notataddvolum").value = note;
+     //delete
+     document.getElementById("deleteeditbutton").style.display = "inline-block";
+     document.getElementById("deleteeditbutton").onclick = function() {
+                    deleteLine(data.airtable);
+                        };
+}
+     
+function reseteditwrapperinput(){
+    document.getElementById("savinglinewrapper").style.display = "none";
+    document.getElementById("listcopyholderlines").style.display = "none";
+    document.getElementById("deleteeditbutton").style.display = "none";
+    document.getElementById("maineditrow").style.display = "grid";
+    document.getElementById('editfunctionbutton').dataset.count = "0";
+    const element = document.getElementById("editornewwrapper");
+     element.removeAttribute('data-airtable');
+    document.getElementById("datevolum").value = makeToDaytring(); 
+    document.getElementById("valueinput").value = "10 000 Kr";
+    document.getElementById("cutinput").value = "10.00 %";
+    document.getElementById("cutinput").style.color = 'black';
+    document.getElementById("cutvalueinput").innerHTML = "1000 Kr";
+    document.getElementById("dropdownInputsupplier").value = "";
+    document.getElementById("dropdownInputsupplier").removeAttribute('data-airtable');
+    document.getElementById("notataddvolum").value = "";
+    document.getElementById("markinput").value = "";
+    document.getElementById("markinput").placeholder = "Merk ...";
+    document.getElementById("bvalueinput").value = "5000 Kr";
+    //flytter editorwrapper vekk fra list element
+    document.getElementById("listparrentholder").appendChild(document.getElementById("editornewwrapper"));
+    //flytte multilineswrapper ut fra list element
+    document.getElementById("listparrentholder").appendChild(document.getElementById("listcopyholderlines"));
+    
+    //ny linjeknappen
+    document.getElementById("addnewlinehandelbutton").style.display = "inline-block";
+    document.getElementById("addnewlinebistandbutton").style.display = "inline-block";
+    document.getElementById("addnewlineanalysebutton").style.display = "inline-block";
+    }
+
+function makeeditfunction(state){
+        
+        const editbuttons = document.getElementsByClassName("editwrapper");
+        
+        for(var i = 0;i<editbuttons.length;i++){
+            if(state){
+            editbuttons[i].style.display = "flex";
+            }else{
+            editbuttons[i].style.display = "none";  
+            }
+        }
+        
+}
+    
+function showhideeditwraper(type){
+    document.getElementById("editornewwrapper").dataset.type = type;
+    
+    if(type == "handel"){
+        document.getElementById("dropdownInputsupplier").style.display = "block";
+        document.getElementById("valueinput").style.display = "block";
+        document.getElementById("cutinput").style.display = "block";
+        document.getElementById("cutvalueinput").style.display = "block";
+        document.getElementById("datevolum").style.display = "block";
+    
+        document.getElementById("markinput").style.display = "none";
+        document.getElementById("bvalueinput").style.display = "none";
+        document.getElementById("avalueinput").style.display = "none";
+        
+    }else if(type == "bistand"){
+        document.getElementById("dropdownInputsupplier").style.display = "block";
+        document.getElementById("markinput").style.display = "block";
+        document.getElementById("bvalueinput").style.display = "block";
+        document.getElementById("datevolum").style.display = "block";
+        
+        document.getElementById("valueinput").style.display = "none";
+        document.getElementById("cutinput").style.display = "none";
+        document.getElementById("cutvalueinput").style.display = "none"; 
+        document.getElementById("avalueinput").style.display = "none";
+        
+    }else if(type == "analyse"){
+        document.getElementById("dropdownInputsupplier").style.display = "block";
+        document.getElementById("markinput").style.display = "block";
+        document.getElementById("avalueinput").style.display = "block";
+        document.getElementById("datevolum").style.display = "block";
+        
+        document.getElementById("bvalueinput").style.display = "none";
+        document.getElementById("valueinput").style.display = "none";
+        document.getElementById("cutinput").style.display = "none";
+        document.getElementById("cutvalueinput").style.display = "none"; 
+        
+    }
+    
+    
+}
+
+function supplierSelected(id,name,cut){
+    
+    //sette hele navnet inn
+    const input = document.getElementById("dropdownInputsupplier");
+    input.value = name;
+    input.dataset.airtable = id;
+
+    // Finn eventuell eksisterende besparelsesfaktor for denne kunde på denne leverandøren
+    const match = SelectedCompanyInFirstTab.cutsettings.find(item => item.supplier === id);
+
+    if (match) {
+        //kunden har en satt rabatt hos denne leverandøren benytt denne
+        document.getElementById("cutinput").value = (match.cut)+"%";
+        //sett fargen til grønn
+        document.getElementById("cutinput").style.color = 'green';
+    } else if (cut) {
+        //kunden har ikke en satt rabatt hos denne leverandøren
+            document.getElementById("cutinput").value = (cut*100)+"%"
+            document.getElementById("cutinput").style.color = '#B8860B';
+    }else{
+        //verken kunden eller leverandøren har en standard verdi
+            document.getElementById("cutinput").style.color = 'black';
+    }
+    
+}
