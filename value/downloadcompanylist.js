8a093f7500f3fd1cf235f6803299047e56175a8b
Kai Bertelsen <kai@exposoft.no>
2025-03-26 13:33:21 +0100

    ny

--- value/downloadcompanylist.js
+++ value/downloadcompanylist.js
@@ -170,7 +170,7 @@ function mainrootcompanylist(data){
     // Filtrere ut alle som har "handel" som type
     let handleList = filterlist.filter(item => item.type === "handel");
 
-    let sum1 = listcompanyinview(mergesuppiersCachflow(groupCahflow));
+    let sum1 = listcompanyinview(groupCahflow);
     //liste opp bistand
     let sum2 = listcompanybistand(findObjectsProperty("type","bistand",filterlist));
     //lister opp analyse
