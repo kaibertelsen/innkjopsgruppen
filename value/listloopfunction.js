6bdc9fa5a7d6265afb6f9030aae1e3771e41e44b
Kai Bertelsen <kai@exposoft.no>
2025-03-28 09:22:23 +0100

    ny

--- value/listloopfunction.js
+++ value/listloopfunction.js
@@ -347,8 +347,7 @@ function loadEditwrapper(data){
             let cutSettingNumber = data.localcut || data.defaultcut || 0;
             cutSetting.value = round(Number(cutSettingNumber)*100, 2)+"%";
 
-            //vise repetisjonsknappen
-            repeatingConteiner.style.display = "flex";
+            
 
         }
         //cutvalue
@@ -415,8 +414,10 @@ function loadEditwrapper(data){
             }
       document.getElementById("markinput").value = mark;
       document.getElementById("avalueinput").value = valutalook(round(data.analysevalue,0))+" Kr";
+
+      //vise repetisjonsknappen
+      repeatingConteiner.style.display = "flex";
     }
-     
      
      
     //datevolum
