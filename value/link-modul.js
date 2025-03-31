66219af0d4a37985bb8c1fdac5245b6e4adc7f8f
Kai Bertelsen <kai@exposoft.no>
2025-03-31 09:17:20 +0200

    ny

--- value/link-modul.js
+++ value/link-modul.js
@@ -6,6 +6,60 @@ document.getElementById("startlinkListButton").onclick = function() {
 }
 
 
-function respondLinkList(data,id){
-    console.log(data);
+function respondLinkList(data){
+  
+    
+    if (!data || !data.fields || !data.fields.followupjson || !Array.isArray(data.fields.followupjson)) {
+        console.error("Ugyldig dataformat: Forventet et objekt med 'fields.followupjson' som en array.");
+        return;
+    }
+    let followup = convertFollowUpJsonStringsToObjects(data.fields.followupjson);
+
+    listLinks(followup);
+
+}
+
+
+function convertFollowUpJsonStringsToObjects(jsonStrings) {
+    return jsonStrings.map((jsonString, index) => {
+        try {
+            // Parse hoved JSON-streng til et objekt
+            const data = JSON.parse(jsonString);
+            if (!data.linkloggjson) {
+                data.linkloggjson = [];
+            } 
+            return data;
+        } catch (error) {
+            console.error(`Feil ved parsing av JSON-streng på indeks ${index}:`, jsonString, error);
+            return null; // Returner null hvis parsing feiler
+        }
+    });
+}
+
+function listLinks(followups){
+    const list = document.getElementById("linklistwrapper");
+    list.innerHTML = "";
+
+    const library = document.getElementById("linklibraryconteiner");
+    const node = library.querySelector(".linkrow");
+
+
+    followups.forEach(followup => {
+        const clone = node.cloneNode(true);
+        clone.querySelector(".companyname").innerText = followup.company || "-";
+        clone.querySelector(".datelinksendt").innerText = followup.date || "-";
+        clone.querySelector(".linkfromuser").innerText = followup.user || "-";
+        clone.querySelector(".linktouser").innerText = followup.email || "-";
+
+
+        clone.querySelector(".linkbutton").onclick = function(){
+            //åpne linken i eget vindu
+            window.open(followup.link);
+        }
+
+        list.appendChild(clone);
+    });
+
+
+    
 }
