f0cb508b4a3ece68813efbc50ad7bcf6a122f0cf
Kai Bertelsen <kai@exposoft.no>
2025-04-28 14:25:00 +0200

    ny

---
+++ importcustomer/startup.js
@@ -0,0 +1,17 @@
+
+
+
+
+document.getElementById("openXlsButton").addEventListener("click", function(event) {
+    event.preventDefault(); 
+
+    const widget = uploadcare.Widget("#logoUploadcareWidget");
+    widget.openDialog().done(function(file) {
+        file.done(function(info) {
+            const optimizedImageURL = info.cdnUrl + "-/format/auto/-/quality/smart/";
+            console.log("Optimalisert bilde URL:", optimizedImageURL);
+            document.getElementById("logoImageCustomer").src = optimizedImageURL;
+            document.getElementById("saveLogoButton").classList.add("active");
+        });
+    });
+});
