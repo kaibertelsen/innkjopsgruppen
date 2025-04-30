09c3aa609f5bdd1d9cf46e1ce252864fb74045b3
Kai Bertelsen <kai@exposoft.no>
2025-04-30 15:39:21 +0200

    ny

--- importcustomer/startup.js
+++ importcustomer/startup.js
@@ -419,3 +419,41 @@ async function multisave(data, baseid, tabelid, returid) {
 function convertMultiResponseData(data) {
     return data.flatMap(samling => samling.map(item => item.fields));
 }
+
+async function POSTairtableMulti(baseId, tableId, body) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const token = await MemberStack.getToken();
+            console.log("Token mottatt:", token);
+
+            let requestBody = body.map(item => ({ fields: { ...item } }));
+
+            console.log("Request Body som skal sendes:", requestBody);
+
+            const response = await fetch(
+                `https://expoapi-zeta.vercel.app/api/row?baseId=${baseId}&tableId=${tableId}&token=${token}`,
+                {
+                    method: "POST",
+                    body: JSON.stringify(requestBody),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+
+            if (!response.ok) {
+                const errorText = await response.text();
+                console.error(`Feilrespons fra API: ${response.status} - ${response.statusText}`);
+                console.error("Responsdata fra API:", errorText);
+                reject(new Error(`HTTP-feil! status: ${response.status} - ${response.statusText}`));
+            } else {
+                const data = await response.json();
+                console.log("Batch lagret med suksess:", data);
+                resolve(data); // Returner responsdata for denne batchen
+            }
+        } catch (error) {
+            console.error("Feil i POSTairtableMulti:", error);
+            reject(error);
+        }
+    });
+}
