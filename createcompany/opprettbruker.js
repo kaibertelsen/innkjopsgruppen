205f5f6a22841ab2c195cf58ec791e91d7559119
Kai Bertelsen <kai@exposoft.no>
2025-05-07 13:54:37 +0200

    ny

--- createcompany/opprettbruker.js
+++ createcompany/opprettbruker.js
@@ -1,10 +1,4 @@
 
-
-document.getElementById("reloadpagebutton").addEventListener("click", () => {
-    location.reload(); // Laster inn nettsiden på nytt
-});
-
-
 function findObjectByKey(globalGroups, searchValue, key) {
     // Bruk Array.find() for å søke etter objektet basert på en spesifikk nøkkel
     const result = globalGroups.find(group => group[key] === searchValue);
